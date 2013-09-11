7962883191ed9ea538d630b3ea820c1781f3c56c
azurenightwalker <azurenightwalker@gmail.com>
2013-09-11 15:48:31 +0100

    Refactor some code

---
+++ MyMedicineReminder/src/main/java/com/mobilemedicsolutions/mymedicinereminder/BaseActivity.java
@@ -0,0 +1,46 @@
+package com.mobilemedicsolutions.mymedicinereminder;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Spinner;
+
+import com.mobilemedicsolutions.mymedicinereminder.data.contentproviders.PreferenceHelper;
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupActionBar();
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}, if the API is available.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setupActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar ab = getActionBar();
+            if (ab != null)
+                ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- MyMedicineReminder/src/main/java/com/mobilemedicsolutions/mymedicinereminder/Configure.java
+++ MyMedicineReminder/src/main/java/com/mobilemedicsolutions/mymedicinereminder/Configure.java
@@ -13,26 +13,12 @@ import android.widget.Spinner;
 
 import com.mobilemedicsolutions.mymedicinereminder.data.contentproviders.PreferenceHelper;
 
-public class Configure extends Activity {
+public class Configure extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure);
-        // Show the Up button in the action bar.
-        setupActionBar();
-    }
-
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setupActionBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            ActionBar ab = getActionBar();
-            if (ab != null)
-                ab.setDisplayHomeAsUpEnabled(true);
-        }
     }
 
     @Override
@@ -40,24 +26,6 @@ public class Configure extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.configure, menu);
         return true;
-    }
-    
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public void savePreferences(@SuppressWarnings("UnusedParameters") View view) {
